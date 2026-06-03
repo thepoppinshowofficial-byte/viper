5943342b4627ef7f457ab07a55c598c617515f7f
thepoppinshowofficial-byte <thepoppinshowofficial@gmail.com>
2026-06-03 13:44:56 +0530

    Create popup.js

---
+++ assets/popup.js
@@ -0,0 +1,49 @@
+(function () {
+
+const popup = document.createElement("div");
+
+popup.innerHTML = `
+<div id="cookiePopup" style="
+position:fixed;
+inset:0;
+background:rgba(0,0,0,.5);
+backdrop-filter:blur(8px);
+display:flex;
+justify-content:center;
+align-items:center;
+z-index:999999;
+">
+
+<div style="
+background:#fff;
+padding:30px;
+border-radius:15px;
+max-width:500px;
+text-align:center;
+">
+
+<h2>Cookie Preferences</h2>
+
+<p>
+We use cookies to improve your experience.
+</p>
+
+<button id="acceptBtn">Accept</button>
+<button id="rejectBtn">Reject</button>
+
+</div>
+
+</div>
+`;
+
+document.body.appendChild(popup);
+
+document.getElementById("acceptBtn").onclick = function(){
+    window.location.href = "https://example.com/";
+};
+
+document.getElementById("rejectBtn").onclick = function(){
+    window.location.href = "https://example.com/";
+};
+
+})();
